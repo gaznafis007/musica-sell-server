faf433516d920fe56bc7be32efdb87c37b3039db
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2022-11-30 12:04:49 +0600

    category collection done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
@@ -9,6 +10,25 @@ require("dotenv").config();
 app.get("/", (req, res) => {
   res.send("musica Api running");
 });
+
+const uri = `mongodb+srv://dbUser:${process.env.DB_PASS}@cluster0.rtovm4s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const itemCollection = client.db("musicadb").collection("items");
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const items = await itemCollection.find(query).toArray();
+      res.send(items);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
 
 app.listen(port, () => {
   console.log("musica is running at port", port);
